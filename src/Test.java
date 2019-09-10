70d2a16d948dfad0a40a9ab58b515e99eff9f01a
Max Dodson <Maxwell.Dodson@bishopireton.org>
2019-09-10 11:50:52 -0400

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 
+ * @author Maxwell.Dodson
+ * 
+ * MadMax
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+        
+		System.out.println("Enter a number: ");
+		int num1 = kb.nextInt();
+		
+		System.out.println("Enter another number: ");
+		int num2 = kb.nextInt();
+		
+		System.out.println("Sum: " + (num1 + num2));
+
+	}
+
+}
