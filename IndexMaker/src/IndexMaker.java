91f35dfd0b9ca910e81a4c7996bb2a96f1d633bc
Max Dodson <Maxwell.Dodson@bishopireton.org>
2019-10-29 13:00:02 -0400

    IndexMaker

---
+++ IndexMaker/src/IndexMaker.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * 
+ * @author Maxwell Dodson
+ * 
+ * @see CardComparator
+ * @see Deck
+ * 
+**/
+import java.util.Scanner;
+public class IndexMaker {
+
+	public static void main(String[] args) {
+	
+		Scanner kb = new Scanner(System.in);
+		//System.out.println("
+	
+	}
+}
