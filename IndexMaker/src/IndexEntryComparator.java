450cf6bda29dd2649e9a255296175561014b76a5
maxdodson <Maxwell.Dodson@bishopireton.org>
2019-11-04 21:23:28 -0500

    Remove Comparator

--- IndexMaker/src/IndexEntryComparator.java
+++
@@ -1,9 +0,0 @@
-/*package IndexMaker.src;
-import java.util.Comparator;
-
-public class IndexEntryComparator implements Comparator<IndexEntry> {
-	public int compare(IndexEntry a, IndexEntry b) {
-		return a.compareToIgnoreCase(b);
-	}
-}
-*/
