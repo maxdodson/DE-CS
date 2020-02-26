15e892a9eeed2e19bd3b7d85488fab4b13f34602
maxdodson <Maxwell.Dodson@bishopireton.org>
2020-02-25 23:03:24 -0500

    Knapsack fixes and updated Javadocs

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -2,12 +2,12 @@
  * Finds the maximum sum of a given set of numbers. It also processes
  * an umbrella test file with individual test files listed inside.
  * 
- * @author Maxwell Dodson
- * 
  * Maxwell Dodson
  * DE CS II
  * 1/23/19
  * Knapsack
+ * 
+ * @author Maxwell Dodson
  * 
  */
 
@@ -28,11 +28,9 @@ public class Knapsack {
 	 * @param fileName the path of the umbrella test file
 	 */
 	public static void processFile(String fileName) {
-	
 		File in = new File(fileName); // Attempt to find provided file
 		try {
 			Scanner input = new Scanner(in);
-			
 			File out = new File(outputFile); // Create output file
 			try {
 				PrintWriter output = new PrintWriter(out);
@@ -58,13 +56,16 @@ public class Knapsack {
 							}
 							
 							ArrayList<Integer> bestWeights = new ArrayList<Integer>();
-							knapsackSum(weights, weights.length, limit, bestWeights); // Obtain best weights
+							// Obtain best weights
+							knapsackSum(weights, weights.length-1, limit, bestWeights);
 							
 							String weightsStr = listWeights(bestWeights);
 							String weightsList = nums.toString();
-							weightsList = weightsList.replaceAll("\\[", "").replaceAll("\\]", ""); // Delete brackets
+							// Delete brackets in weightsList
+							weightsList = weightsList.replaceAll("\\[", "").replaceAll("\\]", "");
 							
-							output.print(f + "\t" + limit + "\t" + weightsList + "\n\n"); // Print output to file
+							// Print output to file
+							output.print(f + "\t" + limit + "\t" + weightsList + "\n\n");
 							output.print(weightsStr + "\n\n");
 							
 						} else {
@@ -79,7 +80,6 @@ public class Knapsack {
 			}
 		} catch (IOException e) { // If file is invalid, prompt user for a new one
 			Scanner kb = new Scanner(System.in);
-			
 			System.out.print("Invalid file. Please enter another: ");
 			String newFile = kb.next();
 			
@@ -90,13 +90,13 @@ public class Knapsack {
 	/**
 	 * Formats weights into a String from the provided List.
 	 * 
+	 * @param weights a List of weights to format as a String
 	 * @return the String list of weights
 	 */
 	public static String listWeights(List<Integer> weights) {
 		if (weights.isEmpty()) {
 			 return nonePossible + object + "s\n";
 		}
-		
 		String str = "";
 		for (Integer i : weights) {
 			str += i + " " + unit + " " + object + "\n";
@@ -110,7 +110,6 @@ public class Knapsack {
 	 * @param args command line arguments; may be a filename
 	 */
 	public static void main(String[] args) {
-		
 		if (args.length != 0) {
 			processFile(args[0]); // Process file provided in command line arguments
 		}
@@ -124,20 +123,21 @@ public class Knapsack {
 	/**
 	 * Recursively finds the maximum sum of a set of numbers, within a given limit.
 	 * 
+	 * @param w the array of weights to evaluate
+	 * @param n the number of positive integers in weights
+	 * @param limit the maximum possible value of the sum of the weights
 	 * @return the integer sum of the optimal weights for the knapsack
 	 */
 	public static int knapsackSum(int[] w, int n, int limit) {
-		
-		if (limit <= 0 || n <= 0) { // Base Case: n has reached beginning of array or limit is 0
+		if (limit <= 0 || n < 0) { // Base Case: n has reached beginning of array or limit is 0
 			return 0;
 		}
-		
-		if (w[n-1] > limit) { // Skip w[n-1] if it is greater than limit
+		if (w[n] > limit) { // Skip w[n] if it is greater than limit
 			return knapsackSum(w, n-1, limit);
 		}
 		
-		int withLast = w[n-1] + knapsackSum(w, n-1, limit-w[n-1]); // Add w[n-1] and succeeding values
-		int withoutLast = knapsackSum(w, n-1, limit); // Skip w[n-1] and only add succeeding values
+		int withLast = w[n] + knapsackSum(w, n-1, limit-w[n]); // Add w[n] and succeeding values
+		int withoutLast = knapsackSum(w, n-1, limit); // Skip w[n] and only add succeeding values
 		
 		return Math.max(withLast, withoutLast); // Return the maximum sum of the weights
 		
@@ -147,25 +147,37 @@ public class Knapsack {
 	 * Recursively finds the maximum sum of a set of numbers, within a given limit.
 	 * The optimal numbers are added to a list.
 	 * 
+	 * @param w the array of weights to evaluate
+	 * @param n the number of positive integers in weights
+	 * @param limit the maximum possible value of the sum of the weights
+	 * @param list an empty List to contain the optimal weights
 	 * @return the integer sum of the optimal weights for the knapsack
 	 */
 	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
-		
-		if (limit <= 0 || n <= 0) { // Base Case: n has reached beginning of array or limit is 0
+		if (limit <= 0 || n < 0) { // Base Case: n has reached beginning of array or limit is 0
 			return 0;
 		}
-		
-		if (w[n-1] > limit) { // Skip w[n-1] if it is greater than limit
+		if (w[n] > limit) { // Skip w[n] if it is greater than limit
 			return knapsackSum(w, n-1, limit, list);
 		}
 		
-		int withLast = w[n-1] + knapsackSum(w, n-1, limit-w[n-1], list); // Add w[n-1] and succeeding values
-		int withoutLast = knapsackSum(w, n-1, limit); // Skip w[n-1] and only add succeeding values
+		List<Integer> listWithLast = new ArrayList<Integer>();
+		List<Integer> listWithoutLast = new ArrayList<Integer>();
+		
+		// Add w[n] and succeeding values
+		int withLast = w[n] + knapsackSum(w, n-1, limit-w[n], listWithLast);
+		listWithLast.add(w[n]);
+		// Skip w[n] and only add succeeding values
+		int withoutLast = knapsackSum(w, n-1, limit, listWithoutLast);
 
-		list.add(w[n-1]); // Add the current best weight to the list
-		return Math.max(withLast, withoutLast); // Return the maximum sum of the weights
+		if (withLast > withoutLast) { // Store optimal weights in list for whichever sum is greater
+			list.addAll(listWithLast);
+			return withLast;
+		} else {
+			list.addAll(listWithoutLast);
+			return withoutLast;
+		}
 	}
-
 }
 
 // Franky helped me and Will understand the problem 
